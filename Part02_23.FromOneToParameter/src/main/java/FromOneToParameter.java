60dc3f6b67404cf770598e88b02030cdd4d1b13d
April Gao <67171198+gaoad@users.noreply.github.com>
2024-03-15 16:00:41 -0400

    Exercises 2-23 to 2-32

--- Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
+++ Part02_23.FromOneToParameter/src/main/java/FromOneToParameter.java
@@ -1,9 +1,18 @@
-
+import java.util.Scanner;
 
 public class FromOneToParameter {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        printUntilNumber(5);
     }
 
+    public static void printUntilNumber(int number) {
+        int i = 1;
+        while (i <= number) {
+            System.out.println(i);
+            i++;
+        }
+    }
 }
